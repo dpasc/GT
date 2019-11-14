10386cb08a73c32ed52388798944c3b9865be06c
dpasc <damianpascale@outlook.com>
2019-11-14 14:55:03 +1100

    Logic added to TravelPackageCityController

--- UI/Areas/Staff/Controllers/TravelPackageCityController.cs
+++ UI/Areas/Staff/Controllers/TravelPackageCityController.cs
@@ -20,7 +20,7 @@ namespace UI.Areas.Staff.Controllers
         }
 
 
-        public async  Task<IActionResult> Index(int id)
+        public async Task<IActionResult> Index(int id)
         {
             ViewBag.TravelPackage = _tpcr.context.TravelPackages
                 .First(tp => tp.Id == id);
@@ -32,7 +32,7 @@ namespace UI.Areas.Staff.Controllers
         {
 
             ViewBag.TravelPackage = _tpcr.context.TravelPackages
-                .First(tp => tp.Id == tpId);
+                .FirstOrDefault(tp => tp.Id == tpId);
             ViewData["Cities"] = new SelectList(_tpcr.context.Cities
                 .OrderBy(tpcr => tpcr.Name), "Id", "Name");
             return View();
@@ -42,7 +42,83 @@ namespace UI.Areas.Staff.Controllers
         public async Task<IActionResult> Create(TravelPackageCity travelPackageCity)
         {
             await _tpcr.Add(travelPackageCity);
-            return RedirectToAction(nameof(Index));
+
+            return RedirectToAction(nameof(Index), new {Id = travelPackageCity.TravelPackageId });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var travelPackageCity = await _tpcr.Get(id);
+            if(travelPackageCity == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.TravelPackage = _tpcr.context.TravelPackages
+              .FirstOrDefault(tp => tp.Id == travelPackageCity.TravelPackageId);
+
+            ViewData["Cities"] = new SelectList(_tpcr.context.Cities
+               .OrderBy(tpcr => tpcr.Name), "Id", "Name");
+            return View(travelPackageCity);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,TravelPackageCity travelPackageCity)
+        {
+            if(id != travelPackageCity.Id)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                await _tpcr.Update(travelPackageCity);
+                return RedirectToAction(nameof(Index), new { Id = travelPackageCity.TravelPackageId });
+
+            }
+            return View(travelPackageCity);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var travelPackageCity = await _tpcr.Get(id);
+            if (travelPackageCity == null)
+            {
+                return NotFound();
+            }
+            ViewBag.TravelPackage = _tpcr.context.TravelPackages
+            .FirstOrDefault(tp => tp.Id == travelPackageCity.TravelPackageId);
+            return View(travelPackageCity);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id, TravelPackageCity travelPackageCity)
+        {
+            if (id != travelPackageCity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _tpcr.Delete(travelPackageCity.Id);
+                return RedirectToAction(nameof(Index), new { Id = travelPackageCity.TravelPackageId });
+
+            }
+            return View(travelPackageCity);
         }
 
 
